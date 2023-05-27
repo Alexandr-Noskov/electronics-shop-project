6e513ab60ab2835feb346997d2dfe5b69da55347
Alexandr Noskov <san9nosko8@gmail.com>
2023-05-27 23:08:01 +0300

    New file

--- tests/test_item.py
+++ tests/test_item.py
@@ -12,6 +12,15 @@ def item():
 def test_calculate_total_price(item):
     assert item.calculate_total_price() == 200000
 
-    def test_apply_discount(item):
+def test_apply_discount(item):
         item.apply_discount()
         assert item.price == 10000.0
+
+
+def test_instantiate_from_csv(cls) -> None:
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
