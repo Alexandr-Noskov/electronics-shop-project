948f2782885d9b3aacf4a2333006f283bc65d7d6
Alexandr Noskov <san9nosko8@gmail.com>
2023-06-04 12:41:25 +0300

    Создан тест для класса Phone

--- tests/test_item.py
+++ tests/test_item.py
@@ -5,10 +5,19 @@ from pytest import fixture
 
 from src.item import Item
 
+from src.phone import Phone
 
-@fixture
+
+@pytest.fixture
 def item():
     return Item("Смартфон", 10000, 20)
+
+@pytest.fixture()
+def make_phone():
+    return Phone("iPhone 14", 120000, 5, 2)
+
+def test_init(item):
+    assert item.quantity == 20
 
 def test_calculate_total_price(item):
     assert Item.calculate_total_price() == 200000
@@ -35,7 +44,16 @@ def test_name_too_long_len(item):
         item.name = 'ТелефонТелефонТелефон'
 
 def test__repr__():
-    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+    assert repr(item) == "Item('Смартфон', 10000, 20)"
 
-def test__str__():
+def test__str__(item1=None):
     assert str(item1) == 'Смартфон'
+
+def test_addition(make_item, make_phone):
+    item1 = make_item
+    phone1 = make_phone
+    assert item1 + phone1 == 25
+    assert phone1 + phone1 == 10
+    with pytest.raises(ValueError):
+        assert item1 + 10 == 40
+        assert phone1 + 5 == 20
