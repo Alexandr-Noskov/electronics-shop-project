45d75c11e9fc6926667da263baf04ef63c349af7
Alexandr Noskov <san9nosko8@gmail.com>
2023-06-12 12:59:43 +0300

    KeyBoard add

---
+++ src/keyboard.py
@@ -0,0 +1,28 @@
+from src.item import Item
+
+
+class MixinLang:
+    __lang = "EN"
+
+
+    @property
+    def language(self):
+        return self.__lang
+
+    def change_lang(self):
+        if self.__lang == "EN":
+            self.__lang = "RU"
+        else:
+            self.__lang = "EN"
+
+        return self
+
+class KeyBoard(MixinLang, Item):
+    def __init__(self, name: str, price: float, quantity: int):
+        super().__init__(name, price, quantity)
+
+
+
+
+keyboard = KeyBoard('Logitech Wireless MX Keys Mini', 1600, 10)
+print(keyboard)
