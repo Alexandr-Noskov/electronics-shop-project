339f36150f0e052d1ebe9eed91ac210e9dab7ea1
Alexandr Noskov <san9nosko8@gmail.com>
2023-06-12 14:34:45 +0300

    тесты для Keyboard

---
+++ tests/test_keyboard.py
@@ -0,0 +1,25 @@
+import pytest
+
+from src.keyboard import KeyBoard
+
+
+def test_init():
+    keyboard = KeyBoard('Logitech Wireless MX Keys Mini', 1600, 10)
+    assert str(keyboard) == "Logitech Wireless MX Keys Mini"
+    assert keyboard.price == 1600
+    assert keyboard.quantity == 10
+
+
+def test_change_lang():
+    keyboard = KeyBoard('Logitech Wireless MX Keys Mini', 1600, 10)
+    assert str(keyboard.language) == 'EN'
+    keyboard.change_lang()
+    assert str(keyboard.language) == 'RU'
+    keyboard.change_lang().change_lang()
+    assert str(keyboard.language) == 'RU'
+
+
+def test_error_setter_language():
+    keyboard = KeyBoard('Logitech Wireless MX Keys Mini', 1600, 10)
+    with pytest.raises(AttributeError):
+        keyboard.lang = 'CH'
